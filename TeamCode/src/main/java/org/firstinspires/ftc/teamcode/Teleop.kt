14e3eb3baf8841702f4bc7fa2133531248685fee
Eric Zheng <greenserpent2007@gmail.com>
2024-01-29 17:26:33 -0500

    Intake and Mec fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.kt
@@ -5,6 +5,7 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.lib.Drone
+import org.firstinspires.ftc.teamcode.lib.Intake
 import org.firstinspires.ftc.teamcode.lib.Mecanum
 
 @TeleOp
@@ -14,15 +15,18 @@ class TeleOp: LinearOpMode() {
         val telemetryMultiple = MultipleTelemetry(telemetry, FtcDashboard.getInstance().telemetry)
         val mecanum = Mecanum(hardwareMap)
         val drone = Drone(hardwareMap)
+        val intake = Intake(hardwareMap)
 
         waitForStart()
 
         while (opModeIsActive()) {
             mecanum.mecanumLoop(gamepad1)
             drone.drone(gamepad1)
+            intake.intakeLoop(gamepad1)
 
             drone.telemetry(telemetryMultiple)
             mecanum.telemetry(telemetryMultiple, gamepad2)
+            intake.telemetry(telemetryMultiple)
             telemetryMultiple.update()
         }
     }
